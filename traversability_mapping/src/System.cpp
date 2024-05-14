b59b5f9afe22aaf394975e1908cc6c26133eb2ca
suchetanrs <suchetan.saravanan@gmail.com>
2024-05-14 14:42:56 +0530

    Working ROS2 Traversability node with additions topic

--- traversability_mapping/src/System.cpp
+++ traversability_mapping/src/System.cpp
@@ -46,6 +46,8 @@ namespace traversability_mapping
                                 long unsigned int mapID)
     {
         sensor_msgs::msg::PointCloud2 sensorPointCloud = pointCloudBuffer_->getClosestPointCloud(timestamp);
+        if(sensorPointCloud.data.size() == 0)
+            return;
         // std::cout << "Got closest pointcloud for: " << kfID << std::endl;
         auto pcl_sec = sensorPointCloud.header.stamp.sec + (sensorPointCloud.header.stamp.nanosec * pow(10, -9));
         // std::cout << "Difference in closest pcl ts is: " << timestamp - pcl_sec << std::endl;
@@ -75,6 +77,8 @@ namespace traversability_mapping
         long unsigned int kfIDlong = static_cast<long unsigned int>(kfID);
 
         sensor_msgs::msg::PointCloud2 sensorPointCloud = pointCloudBuffer_->getClosestPointCloud(timestampDouble);
+        if(sensorPointCloud.data.size() == 0)
+            return;
         // std::cout << "Got closest pointcloud for: " << kfID << std::endl;
         // auto pcl_sec = sensorPointCloud.header.stamp.sec + (sensorPointCloud.header.stamp.nanosec * pow(10, -9));
         // std::cout << "Difference in closest pcl ts is: " << timestamp - pcl_sec << std::endl;
--- traversability_ros_interface/traversability_mapping_ros/src/traversability_node.cpp
+++ traversability_ros_interface/traversability_mapping_ros/src/traversability_node.cpp
@@ -7,6 +7,13 @@
 
 #include "traversability_mapping/System.hpp"
 
+#include <grid_map_core/GridMap.hpp>
+#include <grid_map_core/iterators/GridMapIterator.hpp>
+#include <grid_map_core/GridMap.hpp>
+#include <grid_map_core/iterators/GridMapIterator.hpp>
+#include <grid_map_ros/grid_map_ros.hpp>
+#include <grid_map_cv/grid_map_cv.hpp>
+
 class TraversabilityNode : public rclcpp::Node
 {
 public:
@@ -18,14 +25,29 @@ public:
         this->declare_parameter("updates_topic_name", rclcpp::ParameterValue("/traversability_keyframe_updates"));
         this->get_parameter("updates_topic_name", updates_topic_name_);
 
-        this->declare_parameter("pointcloud_topic_name", rclcpp::ParameterValue("/scout_2/velodyne_points"));
+        this->declare_parameter("pointcloud_topic_name", rclcpp::ParameterValue("/ouster/points"));
         this->get_parameter("pointcloud_topic_name", pointcloud_topic_name_);
+
+        this->declare_parameter("use_lidar_pointcloud", rclcpp::ParameterValue(false));
+        this->get_parameter("use_lidar_pointcloud", use_lidar_pointcloud_);
+        RCLCPP_WARN_STREAM(this->get_logger(), "Using lidar pointcloud? " << use_lidar_pointcloud_);
 
         keyFrameAdditionsSubscriber_ = this->create_subscription<traversability_msgs::msg::KeyFrameAdditions>(
             additions_topic_name_, 10, std::bind(&TraversabilityNode::keyFrameAdditionsCallback, this, std::placeholders::_1));
 
         keyFrameUpdatesSubscriber_ = this->create_subscription<traversability_msgs::msg::KeyFrameUpdates>(
             updates_topic_name_, 10, std::bind(&TraversabilityNode::keyFrameUpdatesCallback, this, std::placeholders::_1));
+
+        lidarPointCloudSubscriber_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+            pointcloud_topic_name_, 10, std::bind(&TraversabilityNode::pointCloudCallback, this, std::placeholders::_1));
+
+        publishTimer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&TraversabilityNode::publishTraversabilityData, this));
+
+        occupancy_grid_publisher_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("local_traversability_map", 10);
+        traversabilityPub_ = this->create_publisher<grid_map_msgs::msg::GridMap>("RTQuadtree_struct", rclcpp::QoS(1).transient_local());
+        pclPublisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("kf_pointcloud", 10);
+        posePublisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("kf_pose", 10);
+
 
         traversabilitySystem_ = std::make_shared<traversability_mapping::System>();
         traversabilitySystem_->addNewLocalMap(0);
@@ -41,11 +63,20 @@ private:
         {
             // Assuming keyframe data structure contains necessary information for mapping
             // Add the keyframe to the traversability map using pTraversability_
-            traversabilitySystem_->addNewLocalMap(keyframe.map_id);
+            if(use_lidar_pointcloud_)
             traversabilitySystem_->addNewKeyFrame(keyframe.kf_timestamp_in_nanosec, keyframe.kf_id, keyframe.map_id);
+            else
+            traversabilitySystem_->addNewKeyFrame(keyframe.kf_timestamp_in_nanosec, keyframe.kf_id, keyframe.map_id, keyframe.kf_pointcloud);
             Eigen::Affine3d keyFramePoseEigen;
             tf2::fromMsg(keyframe.kf_pose, keyFramePoseEigen);
             traversabilitySystem_->updateKeyFrame(keyframe.kf_id, keyFramePoseEigen);
+
+            pclPublisher_->publish(keyframe.kf_pointcloud);
+
+            geometry_msgs::msg::PoseStamped kf_pose_stamped;
+            kf_pose_stamped.header.frame_id = "world";
+            kf_pose_stamped.pose = keyframe.kf_pose;
+            posePublisher_->publish(kf_pose_stamped);
         }
     }
 
@@ -63,12 +94,68 @@ private:
         }
     }
 
+    void pointCloudCallback(sensor_msgs::msg::PointCloud2::SharedPtr msg)
+    {
+        traversabilitySystem_->pushToBuffer(*msg);
+    }
+
+    void publishTraversabilityData()
+    {
+        std::shared_ptr<grid_map::GridMap> traversabilitymap;
+        std::shared_ptr<nav_msgs::msg::OccupancyGrid> gridmap;
+        auto localMap = traversabilitySystem_->getLocalMap();
+        if (localMap != nullptr)
+        {
+            auto keyFramesMap_ = traversabilitySystem_->getLocalMap()->getKeyFramesMap();
+            for (auto &pair : keyFramesMap_)
+            {
+                auto keyFramePtr = pair.second;
+
+                // Check if the pointer is valid before calling recomputeCache
+                if (keyFramePtr)
+                {
+                    auto kfPCL = keyFramePtr->getPointCloud();
+                    auto trav_done = traversabilitySystem_->getLocalMap()->getGridMap();
+                    auto gridmap_done = traversabilitySystem_->getLocalMap()->getOccupancyMap();
+                    if (trav_done)
+                    {
+                        traversabilitymap = trav_done;
+                        gridmap = gridmap_done;
+                    }
+                }
+            }
+            if (traversabilitymap && gridmap)
+            {
+                auto message = *grid_map::GridMapRosConverter::toMessage(*traversabilitymap);
+                occupancy_grid_publisher_->publish(*gridmap);
+                traversabilityPub_->publish(message);
+                return;
+            }
+            else
+            {
+                nav_msgs::msg::OccupancyGrid temp;
+                grid_map_msgs::msg::GridMap temp_grid;
+                return;
+            }
+        }
+        nav_msgs::msg::OccupancyGrid temp;
+        grid_map_msgs::msg::GridMap temp_grid;
+        return;
+    }
+
     std::string additions_topic_name_;
     std::string updates_topic_name_;
     std::string pointcloud_topic_name_;
+    bool use_lidar_pointcloud_;
+    rclcpp::TimerBase::SharedPtr publishTimer_;
 
     rclcpp::Subscription<traversability_msgs::msg::KeyFrameAdditions>::SharedPtr keyFrameAdditionsSubscriber_;
     rclcpp::Subscription<traversability_msgs::msg::KeyFrameUpdates>::SharedPtr keyFrameUpdatesSubscriber_;
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr lidarPointCloudSubscriber_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr occupancy_grid_publisher_;
+    rclcpp::Publisher<grid_map_msgs::msg::GridMap>::SharedPtr traversabilityPub_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pclPublisher_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr posePublisher_;
 
     std::shared_ptr<traversability_mapping::System> traversabilitySystem_;
 };
