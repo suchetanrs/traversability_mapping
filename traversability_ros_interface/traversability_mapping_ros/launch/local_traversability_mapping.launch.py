da701e1fde1fc41ddd8ab96f54fb65f669229189
suchetanrs <suchetan.saravanan@gmail.com>
2025-02-03 22:05:57 +0530

    Fixed local traversability launch file

--- traversability_ros_interface/traversability_mapping_ros/launch/local_traversability_mapping.launch.py
+++ traversability_ros_interface/traversability_mapping_ros/launch/local_traversability_mapping.launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
 
         declare_params_file_cmd = DeclareLaunchArgument(
             'params_file',
-            default_value=os.path.join(traversability_mapping_ros_pkg, 'params', 'traversability_ros_params.yaml'),
+            default_value=os.path.join(traversability_mapping_ros_pkg, 'params', 'traversability_gt_ros_params.yaml'),
             description='Full path to the ROS2 parameters file to use for all launched nodes')
 
         traversability_mapping_ros = Node(
