9f5acc29a27e11df39b605006831b951455272f1
Suchetan Saravanan <suchetan.saravanan@gmail.com>
2026-07-14 18:14:14 +0530

    Remove traversability_mapping_common

--- traversability_mapping/include/traversability_mapping/PointCloudBufferROS.hpp
+++ traversability_mapping/include/traversability_mapping/PointCloudBufferROS.hpp
@@ -21,13 +21,59 @@
 #include <mutex>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/conversions.h>  // PCLPointCloud2, PCLPointField, fromPCLPointCloud2
 #include "traversability_mapping/Helpers.hpp"
-#include "traversability_mapping_common/type_conversion.hpp"
+#ifdef WITH_ROS2_SENSOR_MSGS
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#endif
 
 using namespace std::chrono_literals;
 
 namespace traversability_mapping
 {
+#ifdef WITH_ROS2_SENSOR_MSGS
+    // sensor_msgs -> PCL blob conversion, used only by getClosestPointCloud below.
+    // Hand-rolled (rather than pcl_conversions::toPCL) to keep the core off pcl_ros.
+    inline
+    void toPCL(const sensor_msgs::msg::PointField &pf, pcl::PCLPointField &pcl_pf)
+    {
+        pcl_pf.name = pf.name;
+        pcl_pf.offset = pf.offset;
+        pcl_pf.datatype = pf.datatype;
+        pcl_pf.count = pf.count;
+    }
+
+    inline
+    void toPCL(const std::vector<sensor_msgs::msg::PointField> &pfs, std::vector<pcl::PCLPointField> &pcl_pfs)
+    {
+        pcl_pfs.resize(pfs.size());
+        std::vector<sensor_msgs::msg::PointField>::const_iterator it = pfs.begin();
+        size_t i = 0;
+        for(; it != pfs.end(); ++it, ++i) {
+        toPCL(*(it), pcl_pfs[i]);
+        }
+    }
+
+    inline
+    void copyPointCloud2MetaData(const sensor_msgs::msg::PointCloud2 &pc2, pcl::PCLPointCloud2 &pcl_pc2)
+    {
+        pcl_pc2.height = pc2.height;
+        pcl_pc2.width = pc2.width;
+        toPCL(pc2.fields, pcl_pc2.fields);
+        pcl_pc2.is_bigendian = pc2.is_bigendian;
+        pcl_pc2.point_step = pc2.point_step;
+        pcl_pc2.row_step = pc2.row_step;
+        pcl_pc2.is_dense = pc2.is_dense;
+    }
+
+    inline
+    void toPCL(const sensor_msgs::msg::PointCloud2 &pc2, pcl::PCLPointCloud2 &pcl_pc2)
+    {
+        copyPointCloud2MetaData(pc2, pcl_pc2);
+        pcl_pc2.data = pc2.data;
+    }
+#endif
+
     /**
      * @brief Timestamp-keyed ring buffer holding raw sensor_msgs clouds.
      *
